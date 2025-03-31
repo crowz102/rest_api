b217a3d170a3a39a5327145254766e6b819ddcf5
Lê Thiện <ducthienle95@gmail.com>
2025-03-31 21:50:47 +0700

    Commit JWT, fix port database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5555/mydatabase"
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/mydatabase"
 
 engine = create_engine(DATABASE_URL)
 
