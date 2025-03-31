2f5cd18849f3b69a15d9228e51a85730fb1aff8a
Lê Thiện <ducthienle95@gmail.com>
2025-03-31 22:04:17 +0700

    Install lib and create authen method

---
+++ app/auth.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptoContext
+
+SECRET_KEY = "supermasterkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptoContext(schema=['bcrypt'], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    #Xac minh mat khau da bam voi mat khau nhap vao
+    return pwd_context.verify_password(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    #Tao JWT token tu du lieu nguoi dung
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire}) #Them thoi gian het han vao token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    #Giai ma JWT
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
